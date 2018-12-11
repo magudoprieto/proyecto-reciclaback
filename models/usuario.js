ad4fb9a765438b88d11af6020142ae9ebfddaee9
origin <magudoprieto@gmail.com>
2018-12-11 17:15:57 +0100

    se reciben datos de formulario de registro y se crean usuarios. Además se inscrustan en la BBDD y se muestran en las URL /usuarios/html u usuarios/json

--- models/usuario.js
+++ models/usuario.js
@@ -8,3 +8,10 @@ db.get().query('SELECT * FROM usuarios',(err,rows)=>{
     done(null,rows);
     });
 };
+
+exports.insert = ({ email, contrasena, nombre, edad, municipio, provincia, postal, curso }, done) => {
+    db.get().query('INSERT INTO usuarios VALUES (null, ?, ?, ?, ?, ?, ?, ?, ?)', [email, contrasena, nombre, edad, municipio, provincia, postal, curso], (err, result) => {
+        if (err) return done(err, null)
+        done(null, result)
+    })
+}           
--- routes/api/usuarios.js
+++ routes/api/usuarios.js
@@ -2,13 +2,26 @@ var express = require('express');
 var router = express.Router();
 let usuarioModel = require('../../models/usuario');
 
-router.get('/',(req,res)=>{
+router.get('/:tipo',(req,res)=>{
     usuarioModel.getAll((err,rows) =>{
         console.log(err);
-        res.render('usuarios/main',{
-            arrUsuarios: rows
-        })
+        if (req.params.tipo === 'html') {
+            res.render('usuarios/main',{
+                arrUsuarios: rows
+            })
+        } else {
+            res.json(rows)
+        }
     });
+});
+//aqui recogemos los datos que hemos enviado desde la parte front de angular
+router.post('/json',(req,res)=>{
+    console.log(req.body);
+    usuarioModel.insert(req.body,(err,result)=>{
+        console.log(result);
+        if(err) return res.json({error: err.message})
+    });
+   
 });
 
 module.exports = router;
